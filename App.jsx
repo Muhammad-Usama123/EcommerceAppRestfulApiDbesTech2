ebe841932ecb3be869847b9c25c152be3b3f035b
Muhammad-Usama123 <osamaa.g777@gmail.com>
2023-10-28 00:04:10 +0500

    second push 12:03 leaving office friday 28/10/2023

--- App.jsx
+++ App.jsx
@@ -3,15 +3,56 @@ import React from 'react'
 import { useFonts } from "expo-font"
 import * as SplashScreen from "expo-splash-screen"
 import { useCallback } from 'react'
+import {NavigationContainer} from '@react-navigation/native'
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import BottomTabNavigation from './src/navigation/BottomTabNavigation'
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
+  const [fontsLoaded] = useFonts({
+    regular: require('./App/assets/fonts/Poppins-Regular.ttf'),
+    bold: require('./App/assets/fonts/Poppins-Bold.ttf'),
+    extraBold: require('./App/assets/fonts/Poppins-ExtraBold.ttf'),
+    medium: require('./App/assets/fonts/Poppins-Medium.ttf'),
+    light: require('./App/assets/fonts/Poppins-Light.ttf'),
+    semiBold: require('./App/assets/fonts/Poppins-SemiBold.ttf'),
+  })
+
+const onLayoutRootView = useCallback(async () => {
+  if (fontsLoaded) {
+    await SplashScreen.hideAsync();
+  }
+}, [fontsLoaded])
+
+if (!fontsLoaded) {
+  return null
+}
+
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+        name='Bottom Navigation'
+        component= {BottomTabNavigation}
+        options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
 export default App
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  textStyle: {
+    fontFamily: "regular",
+    fontSize: 20
+  }
+})
---
+++ src/screens/index.js
@@ -0,0 +1,10 @@
+import Home from "./Home";
+import Profile from "./Profile";
+import Search from "./Search";
+
+
+export {
+    Home,
+    Search,
+    Profile
+}
