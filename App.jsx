20e9014c1ad5f7139ed0453ebf49aee21a2106c3
Muhammad-Usama123 <osamaa.g777@gmail.com>
2023-10-27 21:49:32 +0500

    first push of this app friday 27/10/2023 dbes ecommerce app restful api first push after setting up dependencies and folders structure

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { useFonts } from "expo-font"
+import * as SplashScreen from "expo-splash-screen"
+import { useCallback } from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
