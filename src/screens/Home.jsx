a89861b12aee10e1bd0758bbd18aae7fd0edd061
Muhammad-Usama123 <osamaa.g777@gmail.com>
2023-10-29 00:53:10 +0500

    2nd commit of saturday but after 12 am

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,13 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView  } from 'react-native'
 import React from 'react'
 import {SafeAreaView} from 'react-native-safe-area-context'
 import styles from './home.styles'
+import { Ionicons, Fontisto } from 'react-native-vector-icons'
+import { Welcome } from '../components'
+
 
 
 const Home = () => {
   return (
     <SafeAreaView>
-      <Text>Home</Text>
+      <View style= {styles.appBarWrapper}>
+        <View style={styles.appBar}>
+          <Ionicons name='location-outline' size={24}/>
+
+          <Text style={styles.location}> Karachi Pakistan</Text>
+
+          <View style={{ alignItems: 'flex-end'}}>
+            <View style= {styles.cartCount}>
+              <Text style= {styles.cartNumber}> 8 </Text>
+            </View>
+            <TouchableOpacity>
+            <Fontisto name='shopping-bag' size={24} color='black'/>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+      <ScrollView>
+        <Welcome />
+      </ScrollView>
     </SafeAreaView>
   )
 }
